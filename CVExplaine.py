cf2ae6da972940d9930a6bdc80fca31931e8b6a6
cl7paBka <maks-kandikov@yandex.ru>
2024-02-23 17:56:40 +0300

    Switched from api to parsing and deleted translate function

--- CVExplaine.py
+++ CVExplaine.py
@@ -1,9 +1,8 @@
 import sys
 import argparse
-import json
 import requests
 
-from googletrans import Translator
+from bs4 import BeautifulSoup
 
 _exit = sys.exit
 
@@ -23,10 +22,7 @@ def parse_args():
     parser.add_argument('-f', '--file', type=str, help='If you want to insert CVE from .txt file, type filepath here')
     parser.add_argument('-o', '--output', type=str,
                         help='If you want to save the output to .txt file, type filepath here')
-    parser.add_argument('-t', '--translate', action='store_true',
-                        help='Translates CVE description from English to Russian')
     args = parser.parse_args()
-
 
 
 def cve_in_txt(input_path):
@@ -35,69 +31,81 @@ def cve_in_txt(input_path):
             args.CVE += list(i.strip() for i in input_file.readlines())
 
 
-def translate(text):
-    translator = Translator()
-    translated_text = (translator.translate(text, dest='ru')).text.strip()
-    return translated_text
-
-
 def saving_output(output_path, output):
-    if output_path:
+    if output_path is not None:
         with open(output_path, 'w') as output_file:
             output_file.writelines(output)
         print(f"Output saved to {args.output}")
 
 
+def extract_vulnerability_data(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    description = soup.find('p', {'data-testid': 'vuln-description'}).text.strip()
+    date = soup.find('span', {'data-testid': 'vuln-published-on'}).text.strip()
+    cvss_v3_html = soup.find('input', {'id': 'nistV3MetricHidden'})['value']
+    soup_cvss_v3 = BeautifulSoup(cvss_v3_html, 'html.parser')
+    cvss_data = extract_cvss_data(soup_cvss_v3)
+    return {
+        'description': description,
+        'date': date,
+        'cvss': cvss_data
+    }
+
+
+def extract_cvss_data(cvss_v3):
+    cvss_data = {}
+    cvss_data[
+        'base_score'] = f"{cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-base-score'}).text.strip()} {cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-base-score-severity'}).text.strip()}"
+    cvss_data['vector'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-vector'}).text.strip()
+    cvss_data['attack_vector'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-av'}).text.strip()
+    cvss_data['attack_complexity'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-ac'}).text.strip()
+    cvss_data['privileges_required'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-pr'}).text.strip()
+    cvss_data['user_interaction'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-ui'}).text.strip()
+    cvss_data['scope'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-s'}).text.strip()
+    cvss_data['confidentiality'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-c'}).text.strip()
+    cvss_data['integrity'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-i'}).text.strip()
+    cvss_data['availability'] = cvss_v3.find('span', {'data-testid': 'vuln-cvssv3-a'}).text.strip()
+    return cvss_data
+
+
 def run():
-    global result
-
-    result = []
+    output = list()
     for cve_name in args.CVE:
-        api_link = 'https://cve.circl.lu/api/cve/'
-        response = requests.get(api_link + cve_name)
+        url = 'https://nvd.nist.gov/vuln/detail/' + cve_name
+        response = requests.get(url)
         if response.status_code == 200:
-            data = json.loads(response.text)
-            description = data['summary']
-            date = data['Published'][:10]
-
-            information = (f"Name: {cve_name}\n"
-                           f"Date: {date}\n"
-                           f"Link: {'https://nvd.nist.gov/vuln/detail/' + cve_name}\n"
-                           f"Description: {description}\n")
-            if args.translate:
-                translation = translate(description)
-                information += f"Translated: {translation}\n"
-
-            if data['cvss'] is not None:
-                access = data['access']
-                impact = data['impact']
-
-                information += (f"CVSS 2.0 Base Score: {data['cvss']}\n"
-                                f"CVSS Vector: {data['cvss-vector']}\n"
-                                
-                                f"Authentication: {access['authentication']}\n"
-                                f"Complexity: {access['complexity']}\n"
-                                f"Vector: {access['vector']}\n"
-                                
-                                f"Availability: {impact['availability']}\n"
-                                f"Confidentiality: {impact['confidentiality']}\n"
-                                f"Integrity: {impact['integrity']}\n")
-            else:
-                information += f"No CVSS found\n"
-            result.append(information + "\n")
-            print(information)
-
+            html_content = response.content
+            vulnerability_data = extract_vulnerability_data(response.content)
+            cvss = vulnerability_data['cvss']
+            cve_information = (f"{'_' * 30}\n"
+                               f"Name: {cve_name}\n"
+                               f"Date: {vulnerability_data['date']}\n"
+                               f"Link: {url}\n"
+                               f"Description: {vulnerability_data['description']}\n"
+                               f"CVSS v3.0 Base Score: {cvss['base_score']}\n"
+                               f"CVSS Vector: {cvss['vector']}\n"
+                               f"Attack Vector: {cvss['attack_vector']}\n"
+                               f"Attack Complexity: {cvss['attack_complexity']}\n"
+                               f"Privileges Required: {cvss['privileges_required']}\n"
+                               f"User interaction: {cvss['user_interaction']}\n"
+                               f"Scope: {cvss['scope']}\n"
+                               f"Confidentiality: {cvss['confidentiality']}\n"
+                               f"Integrity: {cvss['integrity']}\n"
+                               f"Availability: {cvss['availability']}\n"
+                               f"{'_' * 30}\n")
+            print(cve_information)
+            output.append(cve_information)
         else:
-            print(f"ERROR while receiving file. Status code: {response.status_code}\n"
-                  f"CVE: {cve_name}\n")
+            print("Error:", response.status_code)
     print(f"Done! {len(args.CVE)} CVE processed.")
+    return output
 
 
 def main():
     parse_args()
     cve_in_txt(args.file)
-    run()
-    saving_output(args.output, result)
+    output = run()
+    saving_output(args.output, output)
 
 
 if __name__ == "__main__":
